d0ef005a2c0e0a5b045e1d42055cec990ab2773a
Aly Hirani <alyhirani@Alys-MBP.lan>
2020-10-08 02:20:28 -0500

    Backend cleanup

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-from gitlabstats import GitLabStats
+from gitlab import Stats as GitLabStats
 
 app = Flask(__name__)
 api = Api(app)
--- backend/gitlab.py
+++ backend/gitlab.py
@@ -1,19 +1,19 @@
 from flask_restful import Resource
 import requests
 
-class GitLabStats(Resource):
+class Stats(Resource):
     headers = {'private_token': '1b13PazxJ5_DELouxJ3-'}
 
     commitsURL = "https://gitlab.com/api/v4/projects/21298954/repository/commits?per_page=100"
     issuesURL = "https://gitlab.com/api/v4/projects/21298954/issues?per_page=100"
 
     def get(self):
-        commits = requests.get(GitLabStats.commitsURL, headers=GitLabStats.headers).json()
-        issues = requests.get(GitLabStats.issuesURL, headers=GitLabStats.headers).json()
+        commits = requests.get(self.commitsURL, headers=self.headers).json()
+        issues = requests.get(self.issuesURL, headers=self.headers).json()
 
-        stats = {"Total": {"commits": 0, "issues": 0}}
+        stats = {"total": {"commits": 0, "issues": 0}}
 
-        shortenName = lambda name: name.split()[0]
+        shortenName = lambda name: name.split()[0].lower()
 
         for c in commits:
             name = shortenName(c["committer_name"])
@@ -21,7 +21,7 @@ class GitLabStats(Resource):
                 stats[name]["commits"] += 1
             else:
                 stats[name] = {"commits": 1, "issues": 0}
-            stats["Total"]["commits"] += 1
+            stats["total"]["commits"] += 1
 
         for i in issues:
             if i["state"] == "closed":
@@ -31,6 +31,6 @@ class GitLabStats(Resource):
                         stats[n]["issues"] += 1
                     else:
                         stats[n] = {"commits": 0, "issues": 1}
-                stats["Total"]["issues"] += 1
+                stats["total"]["issues"] += 1
 
-        return [{"name": n, **stats[n]} for n in stats]
+        return stats
