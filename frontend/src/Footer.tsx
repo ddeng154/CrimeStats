906e36bf53329fd1a8d057bf5667d014649a6174
Daniel Deng <ddeng154@gmail.com>
2020-10-17 15:31:35 -0500

    done editing footer for now

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -5,24 +5,25 @@ import './Footer.css';
 function Footer() {
     return (
         <div className = "main-footer">
-            <div className = "container">
+            <div className = "footer-link-wrapper">
                 <div className = "footer-link-items">
                     <h2>Explore</h2>
+                    <Link to = "/">Home</Link>
                     <Link to = "/counties">Counties</Link>
                     <Link to = "/policedepartments">Police Departments</Link>
                     <Link to = "/crimes">Crimes</Link>
                     <Link to = "/about">About</Link>
                 </div>
                 <div className = "footer-text">
-                    <h4>About Us</h4>
+                    <h2>About Us</h2>
                     <p>We want to teach society on the relation between police presence and crime</p>
                 </div>
-                <hr />
-                <div className = "row">
-                    <p className = "col-sm">
-                        &copy;{new Date().getFullYear()} Crimestats | All rights reserved | Terms Of Service | Privacy
-                    </p>
-                </div>
+            </div>
+            <div id="line"><hr /></div>
+            <div className = "footer-cp-wrapper">
+                <p className = "col-sm">
+                    &copy;{new Date().getFullYear()} Crimestats | All rights reserved | Terms Of Service | Privacy
+                </p>
             </div>
         </div>
     )
