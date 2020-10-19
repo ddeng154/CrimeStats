de4679171548ab55a1b7e76c405b585a1c4ede25
Daniel Deng <ddeng154@gmail.com>
2020-10-18 20:38:53 -0500

    added cards

--- frontend/src/Splash.tsx
+++ frontend/src/Splash.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import SplashBanner from './SplashBanner';
 import Cards from './Cards';
 
-
 function Splash() {
     return (
         <div>
             <SplashBanner />
+            <Cards />
         </div>
     );
 }
