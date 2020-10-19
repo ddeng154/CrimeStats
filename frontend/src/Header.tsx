951c6c583b5536dc018e4c57b95148961e32144e
Daniel Deng <ddeng154@gmail.com>
2020-10-19 12:42:18 -0500

    two more files didn't get pushed

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -3,20 +3,20 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 function Header() {
-    return (
-        <div>
-        <Navbar bg="light" expand="sm">
-          <Navbar.Brand href="/">Crime Stats</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="/counties">Counties</Nav.Link>
-            <Nav.Link href="/policedepartments">Police Departments</Nav.Link>
-            <Nav.Link href="/crimes">Crimes</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-          </Nav>
-        </Navbar>
-      </div>
-   
-    )
+  return (
+      <div>
+      <Navbar bg="light" expand="sm">
+        <Navbar.Brand href="/">Crime Stats</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/counties">Counties</Nav.Link>
+          <Nav.Link href="/policedepartments">Police Departments</Nav.Link>
+          <Nav.Link href="/crimes">Crimes</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+        </Nav>
+      </Navbar>
+    </div>
+  
+  )
 }
 
 export default Header;
