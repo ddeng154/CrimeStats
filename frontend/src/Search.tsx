8ac8b5b132737497fe9b44c52aef926d6f63b9dd
Daniel Deng <ddeng154@gmail.com>
2020-10-28 22:22:05 -0500

    Started search function

---
+++ frontend/src/Search.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Search() {
+    return (
+        <div className = "container">
+            <input className = "form-control mb-4" id = "tableSearch" type = "text"
+            placeholder = "Search...">
+
+            </input>
+        </div>
+    )
+}
+
+export default Search;
