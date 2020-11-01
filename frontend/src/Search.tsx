31131ff1532f5c547d3f00cbf674e0ac27a2484e
Daniel Deng <ddeng154@gmail.com>
2020-11-01 10:28:36 -0600

    added hits to search

--- frontend/src/Search.tsx
+++ frontend/src/Search.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 import algoliasearch from 'algoliasearch'
+import './Search.css'
 
 const client = algoliasearch('ICWNC13X5J', '4c22be32a809130f195c1d42981c39d8');
-const players = client.initIndex('players');
-const teams = client.initIndex('teams');
-
 function Search() {
     return (
-        <div className="ais-InstantSearch">
-        <h1>React InstantSearch e-commerce demo</h1>
-        <InstantSearch indexName="demo_ecommerce" searchClient={client}>
+      <div className="ais-InstantSearch">
+        <InstantSearch indexName="dev_CRIMESTATS" searchClient={client}>
           <div className="right-panel">
             <SearchBox />
+            <Hits/>
           </div>
         </InstantSearch>
       </div>
--- frontend/src/SplashBanner.tsx
+++ frontend/src/SplashBanner.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button'
 import './SplashBanner.css';
 import Search from './Search'
 
