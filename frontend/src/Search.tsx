3476ef2c03aad1386ee5108dc1eaf6f4ce843832
Daniel Deng <ddeng154@gmail.com>
2020-10-31 22:08:07 -0500

    search bar added, algolia configured

--- frontend/src/Search.tsx
+++ frontend/src/Search.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import algoliasearch from 'algoliasearch'
+
+const client = algoliasearch('ICWNC13X5J', '4c22be32a809130f195c1d42981c39d8');
+const players = client.initIndex('players');
+const teams = client.initIndex('teams');
 
 function Search() {
     return (
-        <div className = "container">
-            <input className = "form-control mb-4" id = "tableSearch" type = "text"
-            placeholder = "Search...">
-
-            </input>
-        </div>
+        <div className="ais-InstantSearch">
+        <h1>React InstantSearch e-commerce demo</h1>
+        <InstantSearch indexName="demo_ecommerce" searchClient={client}>
+          <div className="right-panel">
+            <SearchBox />
+          </div>
+        </InstantSearch>
+      </div>
     )
 }
+
+
 
 export default Search;
