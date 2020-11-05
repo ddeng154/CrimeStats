b091b1ea90ef9a1b5cb0bd0c2be00ba0a74e3aa3
Shreyas Konana <shreyaskonana@Shreyass-MacBook-Pro.local>
2020-11-04 18:31:44 -0600

    Added chrome options.

--- test/test.py
+++ test/test.py
@@ -8,7 +8,13 @@ import os
 class SeleniumTests(unittest.TestCase):
     def setUp(self):
         # create Chrome session
-        self.driver = webdriver.Chrome("./chromedriver_linux")
+        chrome_path = "./chromedriver_linux"
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument("window-size=1024,768")
+        chrome_options.add_argument("--no-sandbox")
+        self.driver = webdriver.Chrome(chrome_path, chrome_options)
         self.driver.implicitly_wait(10)
         # navigate to webpage
         self.driver.get("http://www.crimestats.me")
